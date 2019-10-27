c54d5a574743df61864b45a48a7c55e756841c60
brendanbond <brendanbond@gmail.com>
2019-10-27 16:24:15 -0500

    who knows

--- server.js
+++ server.js
@@ -13,26 +13,27 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 var authToken = "";
 var refreshToken = "";
 
+function makeRequest(accessCode) {
+  axios.post('https://app.clio.com/oauth/token', {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: "authorization_code",
+      code: accessCode,
+      redirect_uri: "https://clio-multi-timer-server.herokuapp.com/callback"
+    }).then((res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+      authToken = res.query.access_token;
+      refreshToken = res.query.refresh_token;
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+}
 // Clio redirects to here
 app.get('/callback', (req, res) => {
   if (req.query.code) {
-    const accessCode = req.query.code;
-
-    axios.post('https://app.clio.com/oauth/token', {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        grant_type: "authorization_code",
-        code: accessCode,
-        redirect_uri: "https://clio-multi-timer-server.herokuapp.com/callback"
-      }).then((res) => {
-        console.log(`statusCode: ${res.statusCode}`);
-        authToken = res.query.access_token;
-        refreshToken = res.query.refresh_token;
-      })
-      .catch((error) => {
-        console.error(error)
-      });
-
+    console.log("Making request...");
+    makeRequest(req.query.code);
   } else {
     console.log("Error: code not granted");
   }
