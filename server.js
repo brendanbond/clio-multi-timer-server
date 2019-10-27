e65f1aceb4973f4b794b15535632e61676a6bc5b
brendanbond <brendanbond@gmail.com>
2019-10-27 17:09:03 -0500

    trying to fix post request

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ function makeRequest(accessCode) {
     }
   }
 
-  axios.post('https://app.clio.com/oauth/token', querystring.stringify(requestBody))
+  axios.post('https://app.clio.com/oauth/token', `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=authorization_code&code=${accessCode}&redirect_uri=https://clio-multi-timer-server.herokuapp.com/callback`)
     .then((res) => {
       console.log(`statusCode: ${res.statusCode}`);
       authToken = res.query.access_token;
