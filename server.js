39101c1e0092621685612b0375fefe1b662329de
brendanbond <brendanbond@gmail.com>
2019-10-27 16:17:54 -0500

    add post-auth post request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const axios = require('axios');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +10,32 @@ app.use(morgan('dev'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
+var authToken = "";
+var refreshToken = "";
+
+// Clio redirects to here
 app.get('/callback', (req, res) => {
-  console.log("Hello world!");
-  console.log(req.query.code);
+  if (req.query.code) {
+    const accessCode = req.query.code;
+
+    axios.post('https://app.clio.com/oauth/token', {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code: accessCode,
+        redirect_uri: "https://clio-multi-timer-server.herokuapp.com/callback"
+      }).then((res) => {
+        console.log(`statusCode: ${res.statusCode}`);
+        authToken = res.query.access_token;
+        refreshToken = res.query.refresh_token;
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+
+  } else {
+    console.log("Error: code not granted");
+  }
+
   res.send(200);
 });
