9d6df20291d2561b7d89cb2a9d134cae11d7a923
brendanbond <brendanbond@gmail.com>
2019-10-31 14:46:17 -0500

    add cors header

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const port = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
