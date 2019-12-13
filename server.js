edcfae1773a2ca21e56858b73a0bcf6c352e0f2f
brendanbond <brendanbond@gmail.com>
2019-12-13 08:47:43 -0600

    returns a value

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/auth', (req, res) => {
   if (req.query.code) {
     getAccessToken(req.query.code).then((data) => {
       console.log(data);
-      res.send(data.access_token);
+      return data.access_token;
     }).catch((err) => {
       console.log(err);
     });
