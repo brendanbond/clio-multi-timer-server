69df4227b889bad572fc833ab617c8b585746ce3
brendanbond <brendanbond@gmail.com>
2019-12-13 18:27:05 -0600

    more changes to promises

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // Clio auth endpoint
 app.get('/auth', (req, res) => {
   if (req.query.code) {
-    return res.send(getAccessToken(req.query.code));
+    const accessToken = getAccessToken(req.query.code);
+    console.log(accessToken);
+    return res.send(accessToken);
   } else {
     console.log("Error: no authorization code");
   };
